fb25bb074512abc15496c3529f9358b7850e96b3
onticinc <onticinc@gmail.com>
2023-02-06 15:51:07 -0700

    update prisma

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import Footer from './footer'
 import Header from './header'
 import Provider from './provider'
 
-export default function RootLayout({ children }: {children: React.ReactNode}) {
+export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <head />
