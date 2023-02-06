37b5dbbe0e80fd6d4c8dda9dadf0ac37f98f234e
onticinc <onticinc@gmail.com>
2023-02-05 22:15:22 -0700

    setup nextjs and tailwind

---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='p-5 bg-blue-500'><p className='font-bold text-white'>I am the Header</p></div>
+  )
+}
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='text-red-500 font-bold p-5'>I am the HomePage
+    
+    </div>
+  )
+}
+
+export default Home
